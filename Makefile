0e90879a596a4930a8c036f510829e55a5c4aeb3
Ubuntu <ashfaku@JavaMySQLProgram.fenqkdcuahnejkn4pji3daeorf.bx.internal.cloudapp.net>
2021-09-20 23:37:38 +0000

    Updated main.cpp and makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-main: main.o box.o checkerboard.o
-	g++ -o main main.o box.o checkerboard.o
+main: main.o box.o checkerboard.o cross.o lower.o upper.o trapezoid.o
+	g++ -o main main.o box.o checkerboard.o cross.o lower.o upper.o trapezoid.o
 
 main.o: main.cpp box.h
 	g++ -c main.cpp
@@ -10,5 +10,17 @@ box.o: box.cpp box.h
 checkerboard.o: checkerboard.cpp checkerboard.h
 	g++ -c checkerboard.cpp
 
+cross.o: cross.cpp cross.h
+	g++ -c cross.cpp
+
+lower.o: lower.cpp lower.h
+	g++ -c lower.cpp
+
+upper.o: upper.cpp upper.h
+	g++ -c upper.cpp
+
+trapezoid.o: trapezoid.cpp trapezoid.h
+	g++ -c trapezoid.cpp
+
 clean:
-	rm -f main.o box.o checkerboard.o
+	rm -f main.o box.o checkerboard.o cross.o lower.o upper.o trapezoid.o
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "box.h"
 #include "checkerboard.h"
+#include "cross.h"
+#include "lower.h"
+#include "upper.h"
+#include "trapezoid.h"
 using std::cout;
 using std::endl;
 void printBox(int width, int height);
 void printCheckers(int width, int height);
+void printCross(int d);
+void printLowerHalf(int d);
+void printUpperHalf(int d);
+void printTrapezoid(int width, int height);
 int main()
 {
 	printBox(4, 5);
@@ -12,7 +20,40 @@ int main()
 	printBox(9, 10);
 	printCheckers(15, 10);
 	printCheckers(11, 6);
+	printCross(5);
+	printCross(4);
+	printLowerHalf(10);
+	printLowerHalf(5);
+	printUpperHalf(5);
+	printUpperHalf(10);
+	printTrapezoid(12, 5);
+	printTrapezoid(5, 3);
+	printTrapezoid(12, 7);
 	return 0;
+}
+void printTrapezoid(int width, int height)
+{
+	cout << "Trapezoid with a width of " << width << " and a height of " << height << endl;
+	cout << trapezoid(width, height) << endl;
+	cout << "-----------------------------------------------------------------------" << endl;
+}
+void printUpperHalf(int d)
+{
+	cout << "Upper half with dimension " << d << endl;
+	cout << upperhalf(d) << endl;
+	cout << "-----------------------------------------------------------------------" << endl;
+}
+void printLowerHalf(int d)
+{
+	cout << "Lower half with dimension " << d << endl;
+	cout << lowerhalf(d) << endl;
+	cout << "-----------------------------------------------------------------------" << endl;
+}
+void printCross(int d)
+{
+	cout << "Cross with dimension " << d << endl;
+	cout << cross(d) << endl;
+	cout << "-----------------------------------------------------------------------" << endl;
 }
 void printCheckers(int width, int height)
 {
