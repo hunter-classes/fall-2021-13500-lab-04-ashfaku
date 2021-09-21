b4f762a5f6d2513a08c9d97ec31445a4286d5cf1
Ubuntu <ashfaku@JavaMySQLProgram.fenqkdcuahnejkn4pji3daeorf.bx.internal.cloudapp.net>
2021-09-21 13:35:48 +0000

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -17,22 +17,34 @@ void printTrapezoid(int width, int height);
 void print3x3(int width, int height);
 int main()
 {
+	cout << "Task A" << endl;
 	printBox(4, 5);
 	printBox(6, 7);
 	printBox(9, 10);
+	cout << "Task B" << endl;
 	printCheckers(15, 10);
 	printCheckers(11, 6);
+	printCheckers(5, 15);
+	cout << "Task C" << endl;
 	printCross(5);
 	printCross(4);
+	printCross(9);
+	cout << "Task D" << endl;
 	printLowerHalf(10);
 	printLowerHalf(5);
-	printUpperHalf(5);
+	printLowerHalf(3);
+	cout << "Task E" << endl;
 	printUpperHalf(10);
+	printUpperHalf(5);
+	printUpperHalf(3);
+	cout << "Task F" << endl;
 	printTrapezoid(12, 5);
 	printTrapezoid(5, 3);
 	printTrapezoid(12, 7);
+	cout << "Task G" << endl;
 	print3x3(16, 11);
 	print3x3(27, 27);
+	print3x3(8, 5);
 	return 0;
 }
 void print3x3(int width, int height)
